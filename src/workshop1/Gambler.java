a3f9c0b8d56def9ea49beb22ba4bc317bbfd148a
Priya Pandey <96812522+priyapandey1995@users.noreply.github.com>
2022-06-27 20:21:56 +0530

    Update Gambler.java

--- src/workshop1/Gambler.java
+++ src/workshop1/Gambler.java
@@ -5,8 +5,11 @@ public class Gambler {
 	public static void main(String[] args) {
 		//inisialise
 
-		double stake=100;
-		 double bet=1;
+		int stake=100;
+		 int bet=1;
+		System.out.println("Welcome to gambler problem");
+		System.out.println("the stake amount is"+stake);
+		System.out.println("the bet amount is"+bet);
 	}
 
 }
