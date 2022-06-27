436364427fe3ff3d1555cfe0be966f2270beb047
priya <priyapandey8884.blr@gmail.com>
2022-06-27 20:16:16 +0530

    wonOrLoss

--- src/workshop1/Gambler.java
+++ src/workshop1/Gambler.java
@@ -5,8 +5,24 @@ public class Gambler {
 	public static void main(String[] args) {
 		//inisialise
 
-		double stake=100;
-		 double bet=1;
+		int stake=100;
+		 int bets=1;
+		 int totalamountafterbetting;
+		 
+		//when the probability is less than 0.5 its returns true else false
+		 double betting=Math.random();
+		 if(betting <=0.5) {
+			 System.out.println("gambler won the bets");
+			 totalamountafterbetting=stake+bets;
+			 System.out.println("amount is" +" "+ totalamountafterbetting);
+		 }
+			 else {
+				 System.out.println("gambler lost the bets");
+			totalamountafterbetting=stake-bets;
+			 System.out.println("amount is"+" "+totalamountafterbetting);
+			 }
+		 }
+	
 	}
 
-}
+
