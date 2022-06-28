e97a51d8b8224dc92b61a4af440e277d80c9fd3d
priya <priyapandey8884.blr@gmail.com>
2022-06-28 18:41:13 +0530

    UC4

--- src/workshop1/Gambler.java
+++ src/workshop1/Gambler.java
@@ -7,31 +7,39 @@ public class Gambler {
 
 		int stake=100;
 		 int bets=1;
-		 int totalamountafterbetting;
-		 int stakeIncrease = 150;
-		int stakeDecrease = 50;
+		
 		 
 		//when the probability is less than 0.5 its returns true else false
-		while(stake > stakeIncrease && stake < stakeDecrease) {
-		double betting=Math.random();
-		 if(betting <=0.5) {
+		 
+		    int stakeIncrease = 150;
+			int stakeDecrease = 50;
+			int win=0;
+			System.out.println("welcome to gambling world");
+			System.out.println("stake is"+stake);
+			System.out.println("bets is"+bets);
+			for(int day=1;day<=20;day++) {
+			while(stake > stakeIncrease && stake < stakeDecrease) {
+				double betting=Math.random();
+				if(betting <=0.5) {
 			 System.out.println("gambler won the bets");
-			 totalamountafterbetting=stake+bets;
-			 System.out.println("amount is" +" "+ totalamountafterbetting);
-		 }
+			 stake=stake+bets;
+			win=win+50;
+			break;}
 			 else {
 			System.out.println("gambler lost the bets");
-			totalamountafterbetting=stake-bets;
-			 System.out.println("amount is"+" "+totalamountafterbetting);
+			stake=stake-bets;
+			 win=win-50;
+			 break;
 			 }
 		 
-		 
-		 
-		}
+		 }
+			System.out.println("winning amount is"+win);
 			  
 		  }
+		
 		 } 
 	
-	}
+	
+}
 
 
